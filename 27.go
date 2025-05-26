c1dfaa34b3e988293fb76a5f1e44d70aa0542368
dobby-zent <dobby.lee@zent.kr>
2025-05-26 18:18:23 +0900

    wip-371c7313

---
+++ 27.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
+
+/*
+1. 경쟁상태 (atomic 활용)
+*/
+func _27_atomic() {
+	var i int64
+	go func() {
+		atomic.AddInt64(&i, 1)
+	}()
+
+	go func() {
+		atomic.AddInt64(&i, 1)
+	}()
+
+	fmt.Println(i)
+}
+
+/*
+2. 경쟁상태 Critical Section 활용
+*/
+func _27_criticalSection() {
+	var i int64
+
+	var mu sync.Mutex
+
+	go func() {
+		mu.Lock()
+		defer mu.Unlock()
+		i++
+	}()
+
+	go func() {
+		mu.Lock()
+		defer mu.Unlock()
+		i++
+	}()
+
+	fmt.Println(i)
+}
+
+/*
+3. 경쟁상태 (채널 활용)
+*/
+func _27_channel() {
+	var i int64
+	ch := make(chan int64)
+
+	go func() {
+		ch <- 1
+	}()
+
+	go func() {
+		ch <- 1
+	}()
+
+	i += <-ch
+	i += <-ch
+
+	fmt.Println(i)
+}
+
+func main() {
+	_27_atomic()
+	_27_criticalSection()
+	_27_channel()
+}
